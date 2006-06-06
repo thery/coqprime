c42eff67b06b7eaecf7e6c15ed69d1548824283d
bgregoir <bgregoir@4aae0180-a105-0410-8524-f7729f09fccb>
2006-06-06 08:57:43 +0000

    sa marche

--- num/toto.v
+++ num/toto.v
@@ -34,12 +34,38 @@ Definition foo2' n :=
   let v2 := fold_left (fun y x => add_norm (Qq (Arith.Z.of_Z (fst x)) (Arith.Z.to_N (Arith.Z.of_Z (snd x)))) y) (list_prod l1 (rev l1)) zero in
   print v2.
 
+
+Definition myabs x :=
+ match x with
+ |BinInt.Zpos p => p
+ |BinInt.Z0 => xH
+ |BinInt.Zneg p => p
+ end.
+ 
+Definition foo1'' n := 
+  let l1 := map n (BinInt.Zpos BinPos.xH) in
+  let v1 := fold_left (fun y x => add (make_norm (fst x) (myabs (snd x))) y) 
+      (list_prod l1 (rev l1)) zero in
+ print v1.
+
+
+Definition foo2'' n := 
+  let l1 := map n (BinInt.Zpos BinPos.xH) in
+  let v2 := 
+   fold_left 
+     (fun y x => add_norm (make_norm (fst x) (myabs (snd x))) y) 
+     (list_prod l1 (rev l1)) zero in
+  print v2.
+
 Time Eval vm_compute in  (foo1' 1).
+Time Eval vm_compute in  (foo1'' 1).
 Time Eval vm_compute in map 1 (BinInt.Zpos BinPos.xH).
 Time Eval vm_compute in map 2 (BinInt.Zpos BinPos.xH).
 
 Time Eval vm_compute in  (foo1' 2).
 Time Eval vm_compute in  (foo2' 2).
+Time Eval vm_compute in  (foo1'' 2).
+Time Eval vm_compute in  (foo2'' 2).
 
 (* sur ma machine 115 secondes *)
 Time Eval vm_compute in  (foo2' 1).
@@ -48,3 +74,24 @@ Time Eval vm_compute in  compare (foo1 100) (foo2 100).
 (* sur ma machine 6 secondes *)
 
 
+Eval vm_compute in 
+  let l1 := map 2 (BinInt.Zpos BinPos.xH) in
+     (list_prod l1 (rev l1)).
+ 
+
+Fixpoint sum (add:Q.t->Q.t->Q.t) (n d:BinInt.Z) 
+   (dn : nat) (res:Q.t) {struct dn} : Q.t:=
+  match dn with 
+  | 0 => res
+  | S dn =>
+    sum add (Zsucc n) (Zpred d) dn (add res (make_norm n (myabs d)))
+  end.
+
+Definition sum1 n := sum add 1 (Z_of_nat n) n Q.zero.
+Definition sum2 n := sum add_norm 1 (Z_of_nat n) n Q.zero.
+
+Time Eval vm_compute in  print (sum1 2).
+Time Eval vm_compute in  print (sum2 2).
+
+Time Eval vm_compute in  print (sum1 3).
+Time Eval vm_compute in  print (sum2 3).
