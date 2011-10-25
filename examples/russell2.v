cb9941229a89973544716470b784bc402677af44
Laurent Théry <6795190+thery@users.noreply.github.com>
2011-10-25 09:33:51 +0000

    version native

---
+++ examples/russell2.v
@@ -0,0 +1,20 @@
+Require Import PocklingtonRefl.
+
+Set Virtual Machine.
+Open Local Scope positive_scope.
+
+Lemma prime115792089237316195423570985008687907852837564279074904382605163141518161494337 : prime 115792089237316195423570985008687907852837564279074904382605163141518161494337.
+Proof.
+ apply (Pocklington_refl
+         (Pock_certif 115792089237316195423570985008687907852837564279074904382605163141518161494337 5 ((174723607534414371449, 1)::(631, 1)::(149, 1)::(2,6)::nil) 1122621917055392771987235982)
+        ((Pock_certif 174723607534414371449 3 ((4051, 1)::(59, 1)::(17, 1)::(2,3)::nil) 53130590) ::
+         (Proof_certif 4051 prime4051) ::
+         (Proof_certif 631 prime631) ::
+         (Proof_certif 149 prime149) ::
+         (Proof_certif 59 prime59) ::
+         (Proof_certif 17 prime17) ::
+         (Proof_certif 2 prime2) ::
+          nil)).
+ exact_no_check (refl_equal true).
+Qed.
+
