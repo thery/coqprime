84dfee9366efe00de0498919aedd591f017168a0
thery <thery@4aae0180-a105-0410-8524-f7729f09fccb>
2017-10-31 12:15:23 +0000

    copyright

--- elliptic/SMain.v
+++ elliptic/SMain.v
@@ -1,3 +1,10 @@
+(*************************************************************)
+(*      This file is distributed under the terms of the      *)
+(*      GNU Lesser General Public License Version 2.1        *)
+(*************************************************************)
+(*    Benjamin.Gregoire@inria.fr Laurent.Thery@inria.fr      *)
+(*************************************************************)
+
 Require Import Arith_base.
 Require Import Field_tac.
 Require Import Ring.
